bf3de4327512421054f8cf22797f7b302b35ce87
FishHawk <whang177@gmail.com>
2022-08-02 18:00:20 +0800

    support more method

--- README.md
+++ README.md
@@ -8,14 +8,18 @@
 
 1. 将本代码仓库fork到自己的github。
 
+2. 如果想启用健康信息上传，需要在项目目录下添加`xcm.jpg`和`hs.jpg`，分别为行程码和核算截图。
+
 2. 点击Actions选项卡，启用workflow。
 
 3. 点击Settings选项卡，创建两个repository secret.
    - 创建名为`STUID`的secret，值为学号。
    - 创建名为`PASSWORD`的secret，值为统一身份认证密码。
-   - 如果要为多人打卡，可以使用空格作为分隔符。注意学号要与密码数目一致。
+
 4. 默认的打卡时间是每天的早上7:30、中午12:30和晚上19:30，可能会有数分钟的浮动。如需选择其它时间，可以修改`.github/workflows/report.yml`中的`cron`，详细说明参见[触发工作流程的事件](https://docs.github.com/cn/actions/reference/events-that-trigger-workflows#scheduled-events)，请注意这里使用的是**国际标准时间UTC**，北京时间的数值比它大8个小时。建议修改默认时间，避开打卡高峰期以提高成功率。
-5. 在Actions选项卡可以确认打卡情况。如果打卡失败（可能是临时网络问题等原因），脚本会自动重试，三次尝试后如果依然失败，将返回非零值提示构建失败。
+
+5. 在Actions选项卡可以确认打卡情况。如果打卡失败（可能是临时网络问题等原因），脚本会自动重试，三次尝试后如果依然失败，将构建失败。
+
 6. 在Github个人设置页面的Notifications下可以设置Github Actions的通知，建议打开Email通知，并勾选"Send notifications for failed workflows only"。
 
 ### 本地运行
@@ -29,13 +33,17 @@ pip install -r requirements.txt
 脚本的使用方式如下。
 
 ```shell
-usage: report.py [-h] --stuid STUID [STUID ...] --password PASSWORD [PASSWORD ...]
+usage: report.py [-h] --stuid STUID --password PASSWORD [--mrbb] [--jkxxsc] [--sqbb]
 
 USTC auto report script.
 
 options:
-  -h, --help							show this help message and exit
-  --stuid STUID [STUID ...]				your student number
-  --password PASSWORD [PASSWORD ...]	your CAS password
+  -h, --help           show this help message and exit
+  --stuid STUID        你的学号
+  --password PASSWORD  你的统一身份认证的密码
+  --mrbb               执行每日报备.
+  --jkxxsc             执行健康信息上传.
+  --sqbb               执行申请报备(高新校区).
 ```
-其中，`STUID`是学号，`PASSWORD`是统一身份认证的密码。支持多个学号密码，可以使用空格作为分隔符。
+
+其中，健康信息上传会把目录下的`xcm.jpg`和`hs.jpg`文件视作行程码和核算截图。
--- report.py
+++ report.py
@@ -2,15 +2,22 @@
 import argparse
 import requests
 from bs4 import BeautifulSoup
+import re
+import pyjson5
+import datetime
+import pytz
 
 
 def retry(n, function):
     if (function()):
+        print('Successful!')
         return True
     for _ in range(n - 1):
         print('Retrying...')
         if (function()):
+            print('Successful!')
             return True
+    print('Fail!')
     return False
 
 
@@ -19,21 +26,6 @@ class Report(object):
         self.stuid = stuid
         self.password = password
         self.session = requests.Session()
-
-    def run(self):
-        print('Login...')
-        if retry(3, self.login):
-            print('Login Successful!')
-        else:
-            print('Login Failed!')
-            exit(-1)
-
-        print('Report...')
-        if retry(3, self.report):
-            print('Report Successful!')
-        else:
-            print('Report Failed!')
-            exit(-1)
 
     def login(self):
         url = 'https://passport.ustc.edu.cn/login'
@@ -54,27 +46,10 @@ class Report(object):
         })
         return (res.url != url)
 
-    def report(self):
-        url = 'https://weixine.ustc.edu.cn/2020/daliy_report'
-
-        cookies = self.session.cookies
-        headers = {
-            'authority': 'weixine.ustc.edu.cn',
-            'origin': 'https://weixine.ustc.edu.cn',
-            'upgrade-insecure-requests': '1',
-            'content-type': 'application/x-www-form-urlencoded',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'referer': 'https://weixine.ustc.edu.cn/2020/home',
-            'accept-language': 'zh-CN,zh;q=0.9',
-            'Connection': 'close',
-            'cookie': 'PHPSESSID=' + cookies.get('PHPSESSID') + ';XSRF-TOKEN=' + cookies.get('XSRF-TOKEN') + ';laravel_session='+cookies.get('laravel_session'),
-        }
-
+    def 每日报备(self):
         res = self.session.get('https://weixine.ustc.edu.cn/2020')
         soup = BeautifulSoup(res.text, 'html.parser')
         form = soup.find('div', {'id': 'daliy-report'})
-
         data = {}
 
         for row in form.find_all('input', {'type': 'hidden'}):
@@ -93,17 +68,98 @@ class Report(object):
             data[row['name']] = row.contents[0] if len(
                 row.contents) > 0 else ''
 
-        res = self.session.post(url, data=data, headers=headers)
+        url = 'https://weixine.ustc.edu.cn/2020/daliy_report'
+        res = self.session.post(url, data=data)
         return ('上报成功' in res.text)
+
+    def 申请报备高新校区(self):
+        res = self.session.get(
+            'https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        form = soup.find('div', {'id': 'daliy-report'})
+
+        now = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+        data = {
+            'start_date': now.strftime('%Y-%m-%d %H:%m:%S'),
+            'end_date': now.strftime('%Y-%m-%d 23:59:59'),
+            'return_college[]': '高新校区',
+            'reason': '实验室',
+            't': 3,
+        }
+        for row in form.find_all('input', {'type': 'hidden'}):
+            data[row['name']] = row['value']
+
+        url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/ipost'
+        res = self.session.post(url, data=data)
+        return ('报备成功' in res.text)
+
+    def 健康信息上传(self):
+        xcm_filename = 'xcm.jpg'
+        hs_filename = 'hs.jpg'
+
+        res = self.session.get('https://weixine.ustc.edu.cn/2020/upload/xcm')
+        pattern = re.compile(r"formData[^}]*}", re.MULTILINE | re.DOTALL)
+        formData = {}
+        for (name, json) in zip(['行程码', '安康码', '核酸'], re.findall(pattern, str(res.text))):
+            formData[name] = pyjson5.loads(json[len('formData:'):])
+
+        url = 'https://weixine.ustc.edu.cn/2020img/api/upload_for_student'
+
+        xcm_result = True
+        if xcm_filename is not None:
+            data = formData['行程码']
+            files = {'file': open(xcm_filename, 'rb')}
+            res = self.session.post(url, files=files, data=data)
+            xcm_result = res.text.startswith('{"status":true,')
+
+        hs_result = True
+        if hs_filename is not None:
+            data = formData['核酸']
+            files = {'file': open(hs_filename, 'rb')}
+            res = self.session.post(url, files=files, data=data)
+            hs_result = res.text.startswith('{"status":true,')
+
+        return xcm_result and hs_result
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='USTC auto report script.')
-    parser.add_argument('--stuid', nargs='+',
-                        help='your student number', required=True)
-    parser.add_argument('--password', nargs='+',
-                        help='your CAS password', required=True)
+    parser = argparse.ArgumentParser(description='USTC auto report script.')
+    parser.add_argument(
+        '--stuid',  required=True,
+        help='你的学号'
+    )
+    parser.add_argument(
+        '--password', required=True,
+        help='你的统一身份认证的密码'
+    )
+    parser.add_argument(
+        '--mrbb', default=False, action='store_true',
+        help='执行每日报备.'
+    )
+    parser.add_argument(
+        '--jkxxsc', default=False, action='store_true',
+        help='执行健康信息上传.'
+    )
+    parser.add_argument(
+        '--sqbb', default=False, action='store_true',
+        help='执行申请报备(高新校区).'
+    )
     args = parser.parse_args()
-    for stuid, password in zip(args.stuid, args.password):
-        Report(stuid=stuid, password=password).run()
+
+    report = Report(stuid=args.stuid, password=args.password)
+
+    print('Login...')
+    if not retry(3, report.login):
+        exit(-1)
+
+    if args.mrbb:
+        print('每日报备...')
+        retry(3, report.每日报备)
+
+    if args.jkxxsc:
+        print('健康信息上传...')
+        retry(3, report.健康信息上传)
+
+    if args.sqbb:
+        print('申请报备高新校区...')
+        retry(3, report.申请报备高新校区)
